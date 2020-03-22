c1a6d4241954d76e798a4f1bb731f7c5581695f2
Peter Ringering <peteman316@gmail.com>
2020-03-22 13:58:49 -0600

    Converted RSDbLookup.EntityFramework(.NET Framework) to RingSoft.DbLookup.Ef6(.NET Standard)

---
+++ RingSoft.DbLookup.Ef6/ExtensionMethods.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using RingSoft.DbLookup.GetDataProcessor;
+
+namespace RingSoft.DbLookup.Ef6
+{
+    public static class ExtensionMethods
+    {
+        /// <summary>
+        /// Saves the entity.  Outputs all errors to the DbDataProcessor SqlErrorViewer.
+        /// </summary>
+        /// <typeparam name="TEntity">The type of the entity.</typeparam>
+        /// <param name="dbContext">The database context.</param>
+        /// <param name="dbSet">The database set.</param>
+        /// <param name="entity">The entity.</param>
+        /// <param name="debugMessage">The debug message.</param>
+        /// <returns>
+        /// True if no errors occurred during saving.
+        /// </returns>
+        public static bool SaveEntity<TEntity>(this DbContext dbContext, DbSet<TEntity> dbSet, TEntity entity,
+            string debugMessage) where TEntity : class
+        {
+            try
+            {
+                dbSet.AddOrUpdate(entity);
+                dbContext.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                var exception = e;
+                if (exception.InnerException != null)
+                    exception = exception.InnerException;
+                if (exception.InnerException != null)
+                    exception = exception.InnerException;
+
+                DbDataProcessor.DisplayDataException(exception, debugMessage);
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Deletes the entity.  Outputs all errors to the DbDataProcessor SqlErrorViewer.
+        /// </summary>
+        /// <typeparam name="TEntity">The type of the entity.</typeparam>
+        /// <param name="dbContext">The database context.</param>
+        /// <param name="dbSet">The database set.</param>
+        /// <param name="entity">The entity.</param>
+        /// <param name="debugMessage">The debug message.</param>
+        /// <returns></returns>
+        public static bool DeleteEntity<TEntity>(this DbContext dbContext, DbSet<TEntity> dbSet, TEntity entity,
+            string debugMessage) where TEntity : class
+        {
+            try
+            {
+                dbSet.Remove(entity);
+                dbContext.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                var exception = e;
+                if (exception.InnerException != null)
+                    exception = exception.InnerException;
+                if (exception.InnerException != null)
+                    exception = exception.InnerException;
+
+                DbDataProcessor.DisplayDataException(exception, debugMessage);
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ RingSoft.DbLookup.Ef6/LookupContext.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using RingSoft.DbLookup.ModelDefinition;
+using RingSoft.DbLookup.ModelDefinition.FieldDefinitions;
+
+namespace RingSoft.DbLookup.Ef6
+{
+    /// <summary>
+    /// A LookupContextBase derived object that is compatible with the Entity Framework 6.
+    /// </summary>
+    /// <seealso cref="LookupContextBase" />
+    public abstract class LookupContext : LookupContextBase
+    {
+        /// <summary>
+        /// Gets the Entity Framework 6 database context used to set up the table and field definition properties of derived classes.
+        /// </summary>
+        /// <value>
+        /// The database context.
+        /// </value>
+        protected abstract DbContext DbContext { get; }
+
+        protected override void InitializeTableDefinitions()
+        {
+            InitializeEf6TableDefinitions();
+        }
+
+        protected override void InitializeFieldDefinitions()
+        {
+            var objectContext = ((IObjectContextAdapter)DbContext).ObjectContext;
+            var container = objectContext.MetadataWorkspace.GetEntityContainer(objectContext.DefaultContainerName,
+                DataSpace.CSpace);
+
+            foreach (var tableDefinition in TableDefinitions)
+            {
+                var tableProcessingArgs = new TableProcessingArgs(tableDefinition, $"Processing {tableDefinition} Fields");
+                OnTableProcessing(tableProcessingArgs);
+                var entitySet = container.EntitySets.FirstOrDefault(f => f.ElementType.Name == tableDefinition.EntityName);
+                if (entitySet != null)
+                    InitializeFields(entitySet, tableDefinition);
+            }
+
+            InitializeEf6FieldDefinitions();
+        }
+
+        private void InitializeFields(EntitySet entitySet, TableDefinitionBase tableDefinition)
+        {
+            var properties = entitySet.ElementType.DeclaredProperties;
+            foreach (var property in properties)
+            {
+                var fieldDefinition =
+                    tableDefinition.FieldDefinitions.FirstOrDefault(f => f.PropertyName == property.Name);
+                if (fieldDefinition != null)
+                {
+                    fieldDefinition.IsRequired(!property.Nullable);
+                    if (fieldDefinition is StringFieldDefinition stringField)
+                    {
+                        var maxLength = property.MaxLength;
+                        stringField.HasMaxLength(maxLength ?? 0);
+                    }
+                }
+            }
+
+            foreach (var navigationProperty in entitySet.ElementType.NavigationProperties)
+            {
+                InitializeForeignKeys(navigationProperty, tableDefinition);
+            }
+        }
+
+        private void InitializeForeignKeys(NavigationProperty navigationProperty, TableDefinitionBase foreignTableDefinition)
+        {
+            var primaryTableDefinition =
+                foreignTableDefinition.Context.TableDefinitions.FirstOrDefault(t =>
+                    t.EntityName == navigationProperty.TypeUsage.EdmType.Name);
+            if (primaryTableDefinition != null)
+            {
+                var dependentProperties = navigationProperty.GetDependentProperties();
+                var primaryIndex = 0;
+                ForeignKeyDefinition foreignKeyDefinition = null;
+                foreach (var dependentProperty in dependentProperties)
+                {
+                    var foreignFieldDefinition = 
+                        foreignTableDefinition.FieldDefinitions.FirstOrDefault(
+                        f => f.PropertyName == dependentProperty.Name);
+                    var primaryFieldDefinition = primaryTableDefinition.PrimaryKeyFields[primaryIndex];
+
+                    if (foreignFieldDefinition != null)
+                    {
+                        if (foreignKeyDefinition == null)
+                        {
+                            foreignKeyDefinition = foreignFieldDefinition.SetParentField(primaryFieldDefinition, navigationProperty.Name);
+                        }
+                        else
+                        {
+                            foreignKeyDefinition.AddFieldJoin(primaryFieldDefinition, foreignFieldDefinition);
+                        }
+                    }
+
+                    primaryIndex++;
+                }
+            }
+        }
+
+        protected override void InitializePrimaryKeys()
+        {
+            var objectContext = ((IObjectContextAdapter)DbContext).ObjectContext;
+            var container = objectContext.MetadataWorkspace.GetEntityContainer(objectContext.DefaultContainerName,
+                DataSpace.CSpace);
+
+            foreach (var tableDefinition in TableDefinitions)
+            {
+                var entitySet = container.EntitySets.FirstOrDefault(f => f.ElementType.Name == tableDefinition.EntityName);
+                if (entitySet == null)
+                {
+                    throw new Exception($"Table Definition '{tableDefinition.ToString()}' not found as a DbSet in the DbContext object.");
+                }
+
+                SetupEntityPrimaryKey(entitySet, tableDefinition);
+            }
+        }
+
+        private void SetupEntityPrimaryKey(EntitySet entitySet, TableDefinitionBase tableDefinition)
+        {
+            foreach (var keyMember in entitySet.ElementType.KeyMembers)
+            {
+                var fieldDefinition =
+                    tableDefinition.FieldDefinitions.FirstOrDefault(p => p.PropertyName == keyMember.Name);
+                tableDefinition.AddFieldToPrimaryKey(fieldDefinition);
+            }
+        }
+
+        /// <summary>
+        /// Initializes the table definitions.  Derived classes use this to set table definition properties not automatically set up by this class.
+        /// </summary>
+        protected abstract void InitializeEf6TableDefinitions();
+
+        /// <summary>
+        /// Initializes the field definitions.  Derived classes use this to set field definition properties not automatically set up by this class.
+        /// </summary>
+        protected abstract void InitializeEf6FieldDefinitions();
+    }
+}

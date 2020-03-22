80763af411ed0d39f70a050a179007e67e99d5f9
Peter Ringering <peteman316@gmail.com>
2020-03-22 13:27:23 -0600

    Converted RSDbLookup.EntityFrameworkCore to RingSoft.DbLookup.EfCore.

---
+++ RingSoft.DbLookup.EfCore/ExtensionMethods.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using RingSoft.DbLookup.GetDataProcessor;
+
+namespace RingSoft.DbLookup.EfCore
+{
+    public static class ExtensionMethods
+    {
+        /// <summary>
+        /// Adds or updates the entity.  Adding only works if the entity's primary key field is an auto increment type.  Outputs all errors to the DbDataProcessor SqlErrorViewer.
+        /// </summary>
+        /// <typeparam name="TEntity">The type of the entity.</typeparam>
+        /// <param name="context">The context.</param>
+        /// <param name="dbSet">The database set.</param>
+        /// <param name="entity">The entity.</param>
+        /// <param name="debugMessage">The debug message.</param>
+        /// <returns>
+        /// True if no errors occured while saving.
+        /// </returns>
+        public static bool SaveEntity<TEntity>(this DbContext context, DbSet<TEntity> dbSet, TEntity entity,
+            string debugMessage) where TEntity : class
+        {
+            try
+            {
+                dbSet.Update(entity);
+                context.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                var exception = e;
+                if (exception.InnerException != null)
+                    exception = exception.InnerException;
+
+                DbDataProcessor.DisplayDataException(exception, debugMessage);
+
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Adds a new entity.  Use for entities whose primary key field is not an auto-increment type.  Outputs all errors to the DbDataProcessor SqlErrorViewer.
+        /// </summary>
+        /// <typeparam name="TEntity">The type of the entity.</typeparam>
+        /// <param name="context">The context.</param>
+        /// <param name="dbSet">The database set.</param>
+        /// <param name="entity">The entity.</param>
+        /// <param name="debugMessage">The debug message.</param>
+        /// <returns>
+        /// True if no errors occured while saving.
+        /// </returns>
+        public static bool AddNewEntity<TEntity>(this DbContext context, DbSet<TEntity> dbSet, TEntity entity,
+            string debugMessage) where TEntity : class
+        {
+            try
+            {
+                dbSet.Add(entity);
+                context.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                var exception = e;
+                if (exception.InnerException != null)
+                    exception = exception.InnerException;
+
+                DbDataProcessor.DisplayDataException(exception, debugMessage);
+
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Deletes the entity.  Outputs all errors to the DbDataProcessor SqlErrorViewer.
+        /// </summary>
+        /// <typeparam name="TEntity">The type of the entity.</typeparam>
+        /// <param name="dbContext">The database context.</param>
+        /// <param name="dbSet">The database set.</param>
+        /// <param name="entity">The entity.</param>
+        /// <param name="debugMessage">The debug message.</param>
+        /// <returns></returns>
+        public static bool DeleteEntity<TEntity>(this DbContext dbContext, DbSet<TEntity> dbSet, TEntity entity,
+            string debugMessage) where TEntity : class
+        {
+            try
+            {
+                dbSet.Remove(entity);
+                dbContext.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                var exception = e;
+                if (exception.InnerException != null)
+                    exception = exception.InnerException;
+
+                DbDataProcessor.DisplayDataException(exception, debugMessage);
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ RingSoft.DbLookup.EfCore/LookupContext.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using RingSoft.DbLookup.ModelDefinition;
+using RingSoft.DbLookup.ModelDefinition.FieldDefinitions;
+
+namespace RingSoft.DbLookup.EfCore
+{
+    /// <summary>
+    /// A LookupContextBase derived object that is compatible with the Entity Framework Core.
+    /// </summary>
+    /// <seealso cref="LookupContextBase" />
+    public abstract class LookupContext : LookupContextBase
+    {
+
+        /// <summary>
+        /// Gets the Entity Framework Core database context used to set up the table and field definition properties of derived classes.
+        /// </summary>
+        /// <value>
+        /// The database context.
+        /// </value>
+        protected abstract DbContext DbContext { get; }
+
+        protected override void InitializeTableDefinitions()
+        {
+            foreach (var tableDefinition in TableDefinitions)
+            {
+                var entityType = DbContext.Model.FindEntityType(tableDefinition.FullEntityName);
+                if (entityType == null)
+                {
+                    throw new Exception($"Table Definition '{tableDefinition}' not found as a DbSet in the DbContext object.");
+                }
+                else
+                {
+                    tableDefinition.HasTableName(entityType.GetTableName());
+                }
+            }
+            InitializeCoreTableDefinitions();
+        }
+
+
+        protected override void InitializeFieldDefinitions()
+        {
+            foreach (var tableDefinition in TableDefinitions)
+            {
+                var entityType = DbContext.Model.FindEntityType(tableDefinition.FullEntityName);
+                if (entityType != null)
+                    InitializeFields(entityType, tableDefinition);
+            }
+            InitializeCoreFieldDefinitions();
+        }
+
+        private void InitializeFields(IEntityType entityType, TableDefinitionBase tableDefinition)
+        {
+            var properties = entityType.GetProperties();
+            foreach (var property in properties)
+            {
+                var fieldDefinition =
+                    tableDefinition.FieldDefinitions.FirstOrDefault(f => f.PropertyName == property.Name);
+                if (fieldDefinition != null)
+                {
+                    InitializeProperty(property, fieldDefinition);
+                }
+            }
+        }
+
+        private void InitializeProperty(IProperty property, FieldDefinition fieldDefinition)
+        {
+            fieldDefinition.IsRequired(!property.IsNullable);
+            fieldDefinition.HasFieldName(property.GetColumnName());
+            if (fieldDefinition.FieldDataType == FieldDataTypes.String)
+            {
+                if (fieldDefinition is StringFieldDefinition stringField)
+                {
+                    var maxLength = property.GetMaxLength();
+                    stringField.HasMaxLength(maxLength ?? 0);
+                }
+            }
+
+            var foreignKeys = property.GetContainingForeignKeys();
+            foreach (var foreignKey in foreignKeys)
+            {
+                var parentTable =
+                    TableDefinitions.FirstOrDefault(t => t.EntityName == foreignKey.PrincipalEntityType.ClrType.Name);
+                if (parentTable != null)
+                {
+                    ForeignKeyDefinition foreignKeyDefinition = null;
+                    foreach (var foreignKeyProperty in foreignKey.Properties)
+                    {
+                        var principalProperty = foreignKeyProperty.FindFirstPrincipal();
+                        var primaryFieldDefinition =
+                            parentTable.FieldDefinitions.FirstOrDefault(f => f.PropertyName == principalProperty.Name);
+                        if (primaryFieldDefinition != null)
+                        {
+                            if (foreignKeyDefinition == null)
+                            {
+                                foreignKeyDefinition = fieldDefinition.SetParentField(primaryFieldDefinition, foreignKey.DependentToPrincipal.Name);
+                            }
+                            else
+                            {
+                                var foreignFieldDefinition =
+                                    fieldDefinition.TableDefinition.FieldDefinitions.FirstOrDefault(f =>
+                                        f.PropertyName == foreignKeyProperty.Name);
+                                if (foreignFieldDefinition != null)
+                                    foreignKeyDefinition.AddFieldJoin(primaryFieldDefinition, foreignFieldDefinition);
+                            }
+                        }
+                    }
+                }
+                //var primaryProperty = foreignKey.PrincipalKey.Properties[0];
+            }
+        }
+
+        protected override void InitializePrimaryKeys()
+        {
+            foreach (var tableDefinition in TableDefinitions)
+            {
+                var entityType = DbContext.Model.FindEntityType(tableDefinition.FullEntityName);
+                if (entityType != null)
+                    SetupEntityPrimaryKey(entityType, tableDefinition);
+            }
+        }
+
+        private void SetupEntityPrimaryKey(IEntityType entityType, TableDefinitionBase tableDefinition)
+        {
+            var primaryKey = entityType.FindPrimaryKey();
+            if (primaryKey != null)
+            {
+                foreach (var primaryKeyProperty in primaryKey.Properties)
+                {
+                    var fieldDefinition =
+                        tableDefinition.FieldDefinitions.FirstOrDefault(p => p.PropertyName == primaryKeyProperty.Name);
+                    tableDefinition.AddFieldToPrimaryKey(fieldDefinition);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Initializes the table definitions.  Derived classes use this to set table definition properties not automatically set up by this class.
+        /// </summary>
+        protected abstract void InitializeCoreTableDefinitions();
+
+        /// <summary>
+        /// Initializes the field definitions.  Derived classes use this to set field definition properties not automatically set up by this class.
+        /// </summary>
+        protected abstract void InitializeCoreFieldDefinitions();
+    }
+}

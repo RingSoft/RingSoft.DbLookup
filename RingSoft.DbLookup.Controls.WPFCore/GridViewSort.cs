61af80be212156fe5de93fee6da7d0339e591902
Peter Ringering <peteman316@gmail.com>
2020-03-25 18:05:15 -0600

    Converting Controls.WPF to .NET Core.

---
+++ RingSoft.DbLookup.Controls.WPFCore/GridViewSort.cs
@@ -0,0 +1,259 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace RSDbLookup.Controls.Wpf
+{
+    public class GridViewSort
+    {
+        #region Public attached properties
+
+        public static string GetPropertyName(DependencyObject obj)
+        {
+            return (string)obj.GetValue(PropertyNameProperty);
+        }
+
+        public static void SetPropertyName(DependencyObject obj, string value)
+        {
+            obj.SetValue(PropertyNameProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for PropertyName.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PropertyNameProperty =
+            DependencyProperty.RegisterAttached(
+                "PropertyName",
+                typeof(string),
+                typeof(GridViewSort),
+                new UIPropertyMetadata(null)
+            );
+
+        public static bool GetShowSortGlyph(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(ShowSortGlyphProperty);
+        }
+
+        public static void SetShowSortGlyph(DependencyObject obj, bool value)
+        {
+            obj.SetValue(ShowSortGlyphProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for ShowSortGlyph.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ShowSortGlyphProperty =
+            DependencyProperty.RegisterAttached("ShowSortGlyph", typeof(bool), typeof(GridViewSort), new UIPropertyMetadata(true));
+
+        public static ImageSource GetSortGlyphAscending(DependencyObject obj)
+        {
+            return (ImageSource)obj.GetValue(SortGlyphAscendingProperty);
+        }
+
+        public static void SetSortGlyphAscending(DependencyObject obj, ImageSource value)
+        {
+            obj.SetValue(SortGlyphAscendingProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for SortGlyphAscending.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SortGlyphAscendingProperty =
+            DependencyProperty.RegisterAttached("SortGlyphAscending", typeof(ImageSource), typeof(GridViewSort), new UIPropertyMetadata(null));
+
+        public static ImageSource GetSortGlyphDescending(DependencyObject obj)
+        {
+            return (ImageSource)obj.GetValue(SortGlyphDescendingProperty);
+        }
+
+        public static void SetSortGlyphDescending(DependencyObject obj, ImageSource value)
+        {
+            obj.SetValue(SortGlyphDescendingProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for SortGlyphDescending.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SortGlyphDescendingProperty =
+            DependencyProperty.RegisterAttached("SortGlyphDescending", typeof(ImageSource), typeof(GridViewSort), new UIPropertyMetadata(null));
+
+        #endregion
+
+        #region Private attached properties
+
+        private static GridViewColumnHeader GetSortedColumnHeader(DependencyObject obj)
+        {
+            return (GridViewColumnHeader)obj.GetValue(SortedColumnHeaderProperty);
+        }
+
+        private static void SetSortedColumnHeader(DependencyObject obj, GridViewColumnHeader value)
+        {
+            obj.SetValue(SortedColumnHeaderProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for SortedColumn.  This enables animation, styling, binding, etc...
+        private static readonly DependencyProperty SortedColumnHeaderProperty =
+            DependencyProperty.RegisterAttached("SortedColumnHeader", typeof(GridViewColumnHeader), typeof(GridViewSort), new UIPropertyMetadata(null));
+
+        #endregion
+
+        #region Helper methods
+
+        public static void ApplySort(ListSortDirection direction, ListView listView, GridViewColumnHeader sortedColumnHeader)
+        {
+            GridViewColumnHeader currentSortedColumnHeader = GetSortedColumnHeader(listView);
+            if (currentSortedColumnHeader != null)
+            {
+                RemoveSortGlyph(currentSortedColumnHeader);
+                RemoveSortGlyph(sortedColumnHeader);
+            }
+            if (GetShowSortGlyph(listView))
+                AddSortGlyph(
+                    sortedColumnHeader,
+                    direction,
+                    direction == ListSortDirection.Ascending ? GetSortGlyphAscending(listView) : GetSortGlyphDescending(listView));
+            SetSortedColumnHeader(listView, sortedColumnHeader);
+        }
+
+        private static void AddSortGlyph(GridViewColumnHeader columnHeader, ListSortDirection direction, ImageSource sortGlyph)
+        {
+            AdornerLayer adornerLayer = AdornerLayer.GetAdornerLayer(columnHeader);
+            if (adornerLayer != null)
+                adornerLayer.Add(
+                    new SortGlyphAdorner(
+                        columnHeader,
+                        direction,
+                        sortGlyph
+                    ));
+        }
+
+        public static void AddNonPrimarySortGlyph(GridViewColumnHeader columnHeader, int sortIndex)
+        {
+            AdornerLayer adornerLayer = AdornerLayer.GetAdornerLayer(columnHeader);
+            if (adornerLayer != null)
+                adornerLayer.Add(
+                    new SortGlyphAdorner(
+                        columnHeader,
+                        sortIndex
+                    ));
+        }
+
+        public static void RemoveSortGlyph(GridViewColumnHeader columnHeader)
+        {
+            AdornerLayer adornerLayer = AdornerLayer.GetAdornerLayer(columnHeader);
+            if (adornerLayer != null)
+            {
+                Adorner[] adorners = adornerLayer.GetAdorners(columnHeader);
+                if (adorners != null)
+                {
+                    foreach (Adorner adorner in adorners)
+                    {
+                        if (adorner is SortGlyphAdorner)
+                            adornerLayer.Remove(adorner);
+                    }
+                }
+            }
+        }
+
+        public static Size GetGlyphSize(GridViewColumnHeader columnHeader, ListSortDirection direction, ListView listView)
+        {
+            var glyph = direction == ListSortDirection.Ascending ? GetSortGlyphAscending(listView) : GetSortGlyphDescending(listView);
+            if (glyph == null)
+            {
+                var adorner = new SortGlyphAdorner(columnHeader, direction, null);
+                var defaultGlyph = adorner.GetDefaultGlyph();
+                return new Size(defaultGlyph.Bounds.Width, defaultGlyph.Bounds.Height);
+            }
+
+            return new Size(glyph.Width, glyph.Height);
+        }
+
+        #endregion
+
+        #region SortGlyphAdorner nested class
+
+        private class SortGlyphAdorner : Adorner
+        {
+            private GridViewColumnHeader _columnHeader;
+            private ListSortDirection _direction;
+            private ImageSource _sortGlyph;
+            private int _sortIndex;
+
+            public SortGlyphAdorner(GridViewColumnHeader columnHeader, ListSortDirection direction, ImageSource sortGlyph)
+                : base(columnHeader)
+            {
+                _columnHeader = columnHeader;
+                _direction = direction;
+                _sortGlyph = sortGlyph;
+            }
+
+            public SortGlyphAdorner(GridViewColumnHeader columnHeader, int sortIndex) : base(columnHeader)
+            {
+                _columnHeader = columnHeader;
+                _sortIndex = sortIndex;
+            }
+
+            public Geometry GetDefaultGlyph()
+            {
+                double x1 = _columnHeader.ActualWidth / 2 - 5;
+                double x2 = x1 + 10;
+                double x3 = x1 + 5;
+                double y1 = 3;
+                double y2 = y1 + 5;
+
+                if (_direction == ListSortDirection.Ascending)
+                {
+                    double tmp = y1;
+                    y1 = y2;
+                    y2 = tmp;
+                }
+
+                PathSegmentCollection pathSegmentCollection = new PathSegmentCollection();
+                pathSegmentCollection.Add(new LineSegment(new Point(x2, y1), true));
+                pathSegmentCollection.Add(new LineSegment(new Point(x3, y2), true));
+
+                PathFigure pathFigure = new PathFigure(
+                    new Point(x1, y1),
+                    pathSegmentCollection,
+                    true);
+
+                PathFigureCollection pathFigureCollection = new PathFigureCollection();
+                pathFigureCollection.Add(pathFigure);
+
+                PathGeometry pathGeometry = new PathGeometry(pathFigureCollection);
+                return pathGeometry;
+            }
+
+            protected override void OnRender(DrawingContext drawingContext)
+            {
+                base.OnRender(drawingContext);
+
+                if (_sortIndex > 0)
+                {
+                    double x = _columnHeader.ActualWidth / 2 - 5;
+                    double y = 0;
+                    Rect rect = new Rect(x, y, 10, 10);
+                    drawingContext.DrawText(
+                        new FormattedText(_sortIndex.ToString(),
+                            CultureInfo.GetCultureInfo("en-us"),
+                            FlowDirection.LeftToRight,
+                            new Typeface(_columnHeader.FontFamily, _columnHeader.FontStyle, _columnHeader.FontWeight,
+                                _columnHeader.FontStretch),
+                            10, Brushes.Black, new NumberSubstitution(), 1.0),
+                        rect.Location);
+                }
+                else
+                {
+                    if (_sortGlyph != null)
+                    {
+                        double x = _columnHeader.ActualWidth / 2 - 8;
+                        double y = 3;
+                        Rect rect = new Rect(x, y, 16, 16);
+                        drawingContext.DrawImage(_sortGlyph, rect);
+                    }
+                    else
+                    {
+                        drawingContext.DrawGeometry(Brushes.LightGray, new Pen(Brushes.Gray, 1.0), GetDefaultGlyph());
+                    }
+                }
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ RingSoft.DbLookup.Controls.WPFCore/LookupWindow.xaml.cs
@@ -0,0 +1,125 @@
+﻿using RingSoft.DbLookup.Lookup;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using RingSoft.DbLookup.Controls.WPFCore.Annotations;
+
+namespace RSDbLookup.Controls.Wpf
+{
+    /// <summary>
+    /// Interaction logic for LookupWindow.xaml
+    /// </summary>
+    public partial class LookupWindow : INotifyPropertyChanged
+    {
+        public LookupDefinitionBase LookupDefinition
+        {
+            get => _lookupDefinition;
+            set
+            {
+                _lookupDefinition = value;
+                OnPropertyChanged(nameof(LookupDefinition));
+            }
+        }
+
+        /// <summary>
+        /// Occurs when a lookup row is selected by the user.
+        /// </summary>
+        public event EventHandler<LookupSelectArgs> LookupSelect;
+
+        public event EventHandler RefreshData;
+
+        private LookupDefinitionBase _lookupDefinition;
+        private bool _allowView;
+
+        public LookupWindow(LookupDefinitionBase lookupDefinition, bool allowAdd, bool allowView, string initialSearchFor)
+        {
+            LookupDefinition = lookupDefinition;
+            _allowView = allowView;
+
+            InitializeComponent();
+
+            ContentRendered += (sender, args) =>
+            {
+                LookupControl.LookupData.SelectedIndexChanged += LookupData_SelectedIndexChanged;
+                LookupControl.LookupData.LookupView += (o, viewArgs) =>
+                {
+                    SelectButtonClick();
+                    viewArgs.Handled = true;
+                };
+                LookupControl.RefreshData(false, initialSearchFor);
+                MoveFocus(new TraversalRequest(FocusNavigationDirection.First));
+            };
+
+            AddButton.IsEnabled = allowAdd;
+            SelectButton.Click += (sender, args) => { SelectButtonClick(); };
+            AddButton.Click += (sender, args) => AddButtonClick();
+            ViewButton.Click += (sender, args) => ViewButtonClick();
+            CloseButton.Click += (sender, args) => { Close(); };
+
+            PreviewKeyDown += (sender, args) =>
+            {
+                switch (args.Key)
+                {
+                    case Key.Escape:
+                        Close();
+                        args.Handled = true;
+                        break;
+                }
+            };
+        }
+
+        private void LookupData_SelectedIndexChanged(object sender, SelectedIndexChangedEventArgs e)
+        {
+            if (e.NewIndex >= 0)
+            {
+                ViewButton.IsEnabled = _allowView;
+                SelectButton.IsEnabled = true;
+            }
+            else
+            {
+                ViewButton.IsEnabled = SelectButton.IsEnabled = false;
+            }
+        }
+
+        private void SelectButtonClick()
+        {
+            Close();
+            OnSelectLookupRow();
+        }
+
+        protected virtual void OnSelectLookupRow()
+        {
+            var args = new LookupSelectArgs(LookupControl.LookupData);
+            LookupSelect?.Invoke(this, args);
+        }
+
+        private void ViewButtonClick()
+        {
+            var args = new LookupAddViewArgs(LookupControl.LookupData, false, LookupFormModes.View, string.Empty);
+            args.CallBackToken.RefreshData += (sender, eventArgs) => LookupCallBackRefreshData();
+            _lookupDefinition.TableDefinition.Context.OnAddViewLookup(args);
+        }
+
+        private void AddButtonClick()
+        {
+            var args = new LookupAddViewArgs(LookupControl.LookupData, false, LookupFormModes.Add, LookupControl.SearchText);
+            args.CallBackToken.RefreshData += (sender, eventArgs) => LookupCallBackRefreshData();
+            _lookupDefinition.TableDefinition.Context.OnAddViewLookup(args);
+        }
+
+        private void LookupCallBackRefreshData()
+        {
+            LookupControl.LookupData.RefreshData();
+            RefreshData?.Invoke(this, EventArgs.Empty);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}

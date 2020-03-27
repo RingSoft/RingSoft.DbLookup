27e8074a4a2a664e3e9fb4497a8d61fd0ddbc59f
Peter Ringering <peteman316@gmail.com>
2020-03-27 17:05:19 -0600

    Converted unit tests to .NET Core.

---
+++ RingSoft.DbLookup.Tests/DbMaintenance/Ef6ViewModelTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RingSoft.DbLookup.App.Library;
+using RingSoft.DbLookup.App.Library.Ef6;
+using RingSoft.DbLookup.GetDataProcessor;
+
+namespace RingSoft.DbLookup.Tests.DbMaintenance
+{
+    [TestClass]
+    public class Ef6ViewModelTests : DbMaintTestsBase
+    {
+        [ClassInitialize]
+        public static void Setup(TestContext testContext)
+        {
+            DbDataProcessor.SqlErrorViewer = new TestGetDataErrorViewer();
+            RsDbLookupAppGlobals.Initialize();
+            RsDbLookupAppGlobals.EfProcessor = new EfProcessor6();
+            SetupConfigurations();
+        }
+
+        [TestMethod]
+        public void EF6_TestOrderViewModel_DbMaintenanceProcess()
+        {
+            TestOrderViewModel();
+        }
+
+        [TestMethod]
+        public void EF6_TestCustomerViewModel_DbMaintenanceProcess()
+        {
+            TestCustomerViewModel();
+        }
+
+        [TestMethod]
+        public void EF6_TestEmployeeViewModel_DbMaintenanceProcess()
+        {
+            TestEmployeeViewModel();
+        }
+
+        [TestMethod]
+        public void EF6_TestItemViewModel_DbMaintenanceProcess()
+        {
+            TestItemViewModel();
+        }
+    }
+}

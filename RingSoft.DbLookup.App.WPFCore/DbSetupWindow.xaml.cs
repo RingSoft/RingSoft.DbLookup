158ede509e30c13854af2d7e2725abf75f34ad5c
Peter Ringering <peteman316@gmail.com>
2020-04-04 13:53:18 -0600

    Solved error where WinForms designer crashed when trying to design a derived form with base form instantiating a class in a .NET Standard library also targeting .NET Framework.  In .csproj file, net472 needs to be before netstandard2.1.

---
+++ RingSoft.DbLookup.App.WPFCore/DbSetupWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace RingSoft.DbLookup.App.WPFCore
+{
+    /// <summary>
+    /// Interaction logic for DbSetupWindow.xaml
+    /// </summary>
+    public partial class DbSetupWindow
+    {
+        public DbSetupWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- RingSoft.DbLookup.App.WinForms.Forms/DbMaintenanceForm.cs
+++ RingSoft.DbLookup.App.WinForms.Forms/DbMaintenanceForm.cs
@@ -2,42 +2,42 @@
 using RingSoft.DbLookup.App.Library;
 using RingSoft.DbLookup.Lookup;
 using RingSoft.DbLookup.ModelDefinition.FieldDefinitions;
-//using RingSoft.DbMaintenance;
+using RingSoft.DbMaintenance;
 
 namespace RingSoft.DbLookup.App.WinForms.Forms
 {
-    public partial class DbMaintenanceForm : BaseForm
+    public partial class DbMaintenanceForm : BaseForm, IDbMaintenanceView
     {
         public DbMaintenanceForm()
         {
             InitializeComponent();
         }
 
-        //public void OnValidationFail(FieldDefinition fieldDefinition, string text, string caption)
-        //{
-        //    throw new NotImplementedException();
-        //}
+        public void OnValidationFail(FieldDefinition fieldDefinition, string text, string caption)
+        {
+            throw new NotImplementedException();
+        }
 
-        //public void ResetViewForNewRecord()
-        //{
-        //    throw new NotImplementedException();
-        //}
+        public void ResetViewForNewRecord()
+        {
+            throw new NotImplementedException();
+        }
 
-        //public void ShowFindLookupForm(LookupDefinitionBase lookupDefinition, bool allowAdd, bool allowView, string initialSearchFor)
-        //{
-        //    throw new NotImplementedException();
-        //}
+        public void ShowFindLookupForm(LookupDefinitionBase lookupDefinition, bool allowAdd, bool allowView, string initialSearchFor)
+        {
+            throw new NotImplementedException();
+        }
 
-        //public event EventHandler<LookupSelectArgs> LookupFormReturn;
-        //public void CloseWindow()
-        //{
-        //    throw new NotImplementedException();
-        //}
+        public event EventHandler<LookupSelectArgs> LookupFormReturn;
+        public void CloseWindow()
+        {
+            throw new NotImplementedException();
+        }
 
-        //public MessageButtons ShowYesNoCancelMessage(string text, string caption)
-        //{
-        //    throw new NotImplementedException();
-        //}
+        public MessageButtons ShowYesNoCancelMessage(string text, string caption)
+        {
+            throw new NotImplementedException();
+        }
 
         public bool ShowYesNoMessage(string text, string caption)
         {

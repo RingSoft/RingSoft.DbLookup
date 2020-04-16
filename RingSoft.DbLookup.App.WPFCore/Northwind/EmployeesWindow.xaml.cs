0a4f5ae0d421fb4c2cd54988c23020daa4686a6e
Peter Ringering <peteman316@gmail.com>
2020-04-16 12:29:06 -0600

    Finished WPF Employees Window.

--- RingSoft.DbLookup.App.WPFCore/Northwind/EmployeesWindow.xaml.cs
+++ RingSoft.DbLookup.App.WPFCore/Northwind/EmployeesWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using RingSoft.DbLookup.App.Library;
+using RingSoft.DbLookup.ModelDefinition.FieldDefinitions;
 using RingSoft.DbMaintenance;
 
 namespace RingSoft.DbLookup.App.WPFCore.Northwind
@@ -16,6 +17,28 @@ namespace RingSoft.DbLookup.App.WPFCore.Northwind
             InitializeComponent();
 
             Initialize();
+
+            AddModifyButton.Click += (sender, args) => EmployeeViewModel.OnAddModify();
+        }
+        public override void ResetViewForNewRecord()
+        {
+            TabControl.SelectedIndex = 0;
+            FirstNameTextBox.Focus();
+            base.ResetViewForNewRecord();
+        }
+
+        public override void OnValidationFail(FieldDefinition fieldDefinition, string text, string caption)
+        {
+            var table = RsDbLookupAppGlobals.EfProcessor.NorthwindLookupContext.Employees;
+
+            if (fieldDefinition == table.GetFieldDefinition(p => p.FirstName))
+                FirstNameTextBox.Focus();
+            else if (fieldDefinition == table.GetFieldDefinition(p => p.LastName))
+                LastNameTextBox.Focus();
+            else if (fieldDefinition == table.GetFieldDefinition(p => p.ReportsTo))
+                SupervisorControl.Focus();
+
+            base.OnValidationFail(fieldDefinition, text, caption);
         }
     }
 }

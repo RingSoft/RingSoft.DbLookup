2519012e4dbaec1c8832b731a9f040d1f3973de2
Peter Ringering <peteman316@gmail.com>
2022-08-25 13:15:39 -0600

    Working

---
+++ RingSoft.DbLookup.Controls.WPF/FromFormulaHasText.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace RingSoft.DbLookup.Controls.WPF
+{
+    public class FromFormulaHasText : Button
+    {
+        public static readonly DependencyProperty NotificationVisibilityProperty =
+            DependencyProperty.Register(nameof(NotificationVisibility), typeof(Visibility), typeof(FromFormulaHasText),
+                new FrameworkPropertyMetadata(Visibility.Collapsed));
+
+        public Visibility NotificationVisibility
+        {
+            get { return (Visibility)GetValue(NotificationVisibilityProperty); }
+            set { SetValue(NotificationVisibilityProperty, value); }
+        }
+
+        public static readonly DependencyProperty MemoHasTextProperty =
+            DependencyProperty.Register(nameof(MemoHasText), typeof(bool), typeof(FromFormulaHasText));
+
+        public bool MemoHasText
+        {
+            get { return (bool)GetValue(MemoHasTextProperty); }
+            set { SetValue(MemoHasTextProperty, value); }
+        }
+
+        static FromFormulaHasText()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(FromFormulaHasText), new FrameworkPropertyMetadata(typeof(FromFormulaHasText)));
+        }
+
+    }
+}

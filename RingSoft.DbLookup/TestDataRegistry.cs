991b794ab5677fd8e61f3a213775b3c7be01bfb0
Peter Ringering <peteman316@gmail.com>
2023-05-22 21:47:59 -0600

    Working

---
+++ RingSoft.DbLookup/TestDataRegistry.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using RingSoft.DbLookup.Lookup;
+
+namespace RingSoft.DbLookup
+{
+    public abstract class DataRepositoryRegistryItemBase
+    {
+        public Type Entity { get; internal set; }
+
+        public abstract void ClearData();
+    }
+
+    public class DataRepositoryRegistryItem<TEntity> : DataRepositoryRegistryItemBase where TEntity : class
+    {
+        public List<TEntity> Table { get; private set; }
+
+        public DataRepositoryRegistryItem(TEntity entity)
+        {
+            Table = new List<TEntity>();
+            Entity = typeof(TEntity);
+        }
+
+        public override void ClearData()
+        {
+            Table.Clear();
+        }
+    }
+
+    public class DataRepositoryRegistry : IDbContext
+    {
+        public List<DataRepositoryRegistryItemBase> Entities { get; private set; } =
+            new List<DataRepositoryRegistryItemBase>();
+
+        public DataRepositoryRegistry DbContext { get; private set; }
+
+        public DataRepositoryRegistry()
+        {
+        }
+
+        public void AddEntity(DataRepositoryRegistryItemBase entity)
+        {
+            Entities.Add(entity);
+        }
+
+        public List<TEntity> GetList<TEntity>() where TEntity : class
+        {
+            var result = new List<TEntity>();
+            var entity = Entities.FirstOrDefault(p => p.Entity == typeof(TEntity));
+            if (entity != null)
+            {
+                var existingEntity = entity as DataRepositoryRegistryItem<TEntity>;
+                return existingEntity.Table;
+            }
+
+            return result;
+        }
+
+        public IQueryable<TEntity> GetEntity<TEntity>() where TEntity : class
+        {
+            var entity = Entities.FirstOrDefault(p => p.Entity == typeof(TEntity));
+            if (entity != null)
+            {
+                var existingEntity = entity as DataRepositoryRegistryItem<TEntity>;
+                return existingEntity.Table.AsQueryable();
+            }
+
+            return null;
+        }
+
+        public bool SaveNoCommitEntity<TEntity>(TEntity entity, string message) where TEntity : class
+        {
+            var table = GetList<TEntity>();
+            if (!table.Contains(entity))
+            {
+                table.Add(entity);
+            }
+            return true;
+        }
+
+        public bool SaveEntity<TEntity>(TEntity entity, string message) where TEntity : class
+        {
+            return SaveNoCommitEntity(entity, message);
+        }
+
+        public bool DeleteEntity<TEntity>(TEntity entity, string message) where TEntity : class
+        {
+            var table = GetList<TEntity>();
+            if (table.Contains(entity))
+            {
+                table.Remove(entity);
+            }
+            return true;
+        }
+
+        public bool DeleteNoCommitEntity<TEntity>(TEntity entity, string message) where TEntity : class
+        {
+            return DeleteEntity(entity, message);
+        }
+
+        public bool AddNewNoCommitEntity<TEntity>(TEntity entity, string message) where TEntity : class
+        {
+            return SaveNoCommitEntity(entity, message);
+        }
+
+        public bool Commit(string message)
+        {
+            return true;
+        }
+
+        public void RemoveRange<TEntity>(IEnumerable<TEntity> listToRemove) where TEntity : class
+        {
+            var table = GetList<TEntity>();
+            foreach (var entity in listToRemove)
+            {
+                if (table.Contains(entity))
+                {
+                    table.Remove(entity);
+                }
+            }
+        }
+
+        public void AddRange<TEntity>(List<TEntity> listToAdd) where TEntity : class
+        {
+            var table = GetList<TEntity>();
+            table.AddRange(listToAdd);
+        }
+
+        public IQueryable<TEntity> GetTable<TEntity>() where TEntity : class
+        {
+            return GetList<TEntity>().AsQueryable();
+        }
+    }
+
+    public class TestDataRepository : IDataRepository
+    {
+        public DataRepositoryRegistry DataContext { get; }
+        public TestDataRepository(DataRepositoryRegistry context)
+        {
+            DataContext = context;
+        }
+
+        IDbContext DbLookup.IDataRepository.GetDataContext()
+        {
+            return DataContext;
+        }
+
+        public ILookupDataBase GetLookupDataBase<TEntity>(LookupDefinitionBase lookupDefinition, LookupUserInterface lookupUi) where TEntity : class, new()
+        {
+            return new TestLookupDataBase<TEntity>(DataContext.GetTable<TEntity>());
+        }
+
+        public void ClearData()
+        {
+            foreach (var entity in DataContext.Entities)
+            {
+                entity.ClearData();
+            }
+        }
+
+    }
+}
---
+++ RingSoft.DbMaintenance/DbMaintenanceTestGlobals.cs
@@ -0,0 +1,146 @@
+﻿using RingSoft.DataEntryControls.Engine;
+using RingSoft.DbLookup;
+using System;
+using RingSoft.DbLookup.Lookup;
+using RingSoft.DbLookup.ModelDefinition.FieldDefinitions;
+
+namespace RingSoft.DbMaintenance
+{
+    public class DbMaintenanceTestGlobals<TViewModel, TView> 
+        : IControlsUserInterface
+    , IDbMaintenanceDataProcessor
+
+        where TViewModel : DbMaintenanceViewModelBase
+        where TView : IDbMaintenanceView, new()
+    {
+        public TViewModel ViewModel { get; private set; }
+
+        public TView View { get; private set; }
+
+
+        public TestDataRepository DataRepository { get; }
+        public DbMaintenanceTestGlobals(TestDataRepository dataDepository)
+        {
+            DataRepository = dataDepository;
+        }
+
+        public virtual void Initialize()
+        {
+            ControlsGlobals.UserInterface = this;
+
+            var viewModel = (TViewModel)Activator.CreateInstance(typeof(TViewModel));
+            ViewModel = viewModel;
+            ViewModel.Processor = this;
+
+            var view = (TView)Activator.CreateInstance(typeof(TView));
+            View = view;
+
+            ViewModel.OnViewLoaded(View);
+        }
+
+        public virtual void ClearData()
+        {
+
+            ViewModel.NewCommand.Execute(null);
+            DataRepository.ClearData();
+        }
+
+        public void SetWindowCursor(WindowCursorTypes cursor)
+        {
+
+        }
+
+        public void ShowMessageBox(string text, string caption, RsMessageBoxIcons icon)
+        {
+
+        }
+
+        public MessageBoxButtonsResult ShowYesNoMessageBox(string text, string caption, bool playSound = false)
+        {
+            return MessageBoxButtonsResult.Yes;
+        }
+
+        public MessageBoxButtonsResult ShowYesNoCancelMessageBox(string text, string caption, bool playSound = false)
+        {
+            return MessageBoxButtonsResult.Yes;
+        }
+
+        public bool KeyControlRegistered { get; set; }
+        public event EventHandler<LookupAddViewArgs> LookupAddView;
+        public void InitializeFromLookupData(LookupAddViewArgs e)
+        {
+            
+        }
+
+        public void OnValidationFail(FieldDefinition fieldDefinition, string text, string caption)
+        {
+            
+        }
+
+        public void OnRecordSelected()
+        {
+            
+        }
+
+        public void ShowFindLookupWindow(LookupDefinitionBase lookupDefinition, bool allowAdd, bool allowView, string initialSearchFor,
+            PrimaryKeyValue initialSearchForPrimaryKey)
+        {
+            
+        }
+
+        public void CloseWindow()
+        {
+            
+        }
+
+        public MessageButtons ShowYesNoCancelMessage(string text, string caption, bool playSound = false)
+        {
+            return MessageButtons.Yes;
+        }
+
+        public bool ShowYesNoMessage(string text, string caption, bool playSound = false)
+        {
+            return true;
+        }
+
+        public void ShowRecordSavedMessage()
+        {
+            
+        }
+
+        public void OnReadOnlyModeSet(bool readOnlyValue)
+        {
+            
+        }
+
+        public bool IsMaintenanceKeyDown(MaintenanceKey key)
+        {
+            return false;
+        }
+
+        public void Activate()
+        {
+            
+        }
+
+        public void SetWindowReadOnlyMode()
+        {
+            
+        }
+
+        public bool ShowRecordLockWindow(PrimaryKeyValue lockKey, string message, object inputParameter)
+        {
+            return true;
+        }
+
+        public bool CheckDeleteTables(DeleteTables deleteTables)
+        {
+            return true;
+        }
+
+        public void PrintOutput(PrinterSetupArgs printerSetupArgs)
+        {
+            
+        }
+    }
+}

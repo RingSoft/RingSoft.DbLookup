403d4109c9ab789931756059578919a812440899
Peter Ringering <peteman316@gmail.com>
2023-11-21 19:57:30 -0700

    Working

--- RingSoft.DbLookup.App.Library/Northwind/INorthwindEfDataProcessor.cs
+++ RingSoft.DbLookup.App.Library/Northwind/INorthwindEfDataProcessor.cs
@@ -8,39 +8,5 @@ namespace RingSoft.DbLookup.App.Library.Northwind
         void CheckDataExists();
 
         void SetDataContext();
-
-        Customer GetCustomer(string customerId);
-
-        bool SaveCustomer(Customer customer, bool addMode);
-
-        bool DeleteCustomer(string customerId);
-
-        Employee GetEmployee(int employeeId);
-
-        bool SaveEmployee(Employee employee);
-
-        bool DeleteEmployee(int employeeId);
-
-        Order GetOrder(int orderId, bool gridMode);
-
-        bool SaveOrder(Order order, List<Order_Detail> details);
-
-        bool DeleteOrder(int orderId);
-
-        List<Order_Detail> GetOrderDetails(int orderId);
-
-        Order_Detail GetOrderDetail(int orderId, int productId);
-
-        bool SaveOrderDetail(Order_Detail orderDetail);
-
-        bool DeleteOrderDetail(int orderId, int productId);
-
-        Product GetProduct(int productId);
-
-        bool SaveProduct(Product product);
-
-        bool DeleteProduct(int productId);
-
-        Shipper GetShipper(int shipperId);
     }
 }

f7203e6681915aae64c6e2c1355bfdf6e5c29afb
Peter Ringering <peteman316@gmail.com>
2023-11-23 20:17:31 -0700

    Working

--- RingSoft.DbLookup.EfCore/DbConstants.cs
+++ RingSoft.DbLookup.EfCore/DbConstants.cs
@@ -5,14 +5,6 @@ namespace RingSoft.DbLookup.EfCore
 {
     public class DbConstants
     {
-        //public const string IntegerColumnType = "integer";
-        //public const string StringColumnType = "nvarchar";
-        //public const string DecimalColumnType = "numeric";
-        //public const string DateColumnType = "datetime";
-        //public const string ByteColumnType = "tinyint";
-        //public const string BoolColumnType = "bit";
-        //public const string MemoColumnType = "ntext";
-
         public static DbFieldConstants ConstantGenerator { get; set; }
 
         public static string IntegerColumnType

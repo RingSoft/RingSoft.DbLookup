0f2bea456201246900b8416da7124bc788486526
Peter Ringering <peteman316@gmail.com>
2023-12-18 01:29:24 -0700

    Working

--- RingSoft.DbLookup.App.Library/Northwind/LookupModel/OrderLookup.cs
+++ RingSoft.DbLookup.App.Library/Northwind/LookupModel/OrderLookup.cs
@@ -4,8 +4,6 @@ namespace RingSoft.DbLookup.App.Library.Northwind.LookupModel
 {
     public class OrderLookup
     {
-        public int OrderId { get; set; }
-
         public string Order { get; set; }
 
         public string Customer { get; set; }

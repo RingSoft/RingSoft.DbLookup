59e2a31d0213432287d9c947d148f52c76275516
Peter Ringering <peteman316@gmail.com>
2024-06-26 20:24:49 -0600

    Working

--- RingSoft.DbLookup.App.WPFCore/AppFrom Downloads.bat
+++ RingSoft.DbLookup.App.WPFCore/AppFrom Downloads.bat
@@ -1,3 +1,3 @@
 move C:\Users\petem\Downloads\RingSoft.DbLookup.App.WPFCore.exe C:\Users\petem\source\repos\RingSoft\RingSoft.DbLookup\RingSoft.DbLookup.App.WPFCore\bin\Release\net8.0-windows7.0
-delete C:\Users\petem\source\repos\RingSoft\Nuget\Release\CodeSign\RingSoft.DbLookup.App.WPFCore.exe
+del C:\Users\petem\source\repos\RingSoft\Nuget\Release\CodeSign\RingSoft.DbLookup.App.WPFCore.exe
 pause
--- RingSoft.DbLookup.App.WPFCore/InstallFrom Downloads.bat
+++ RingSoft.DbLookup.App.WPFCore/InstallFrom Downloads.bat
@@ -1,3 +1,3 @@
 move C:\Users\petem\Downloads\RingSoft.WPFDbLookupDemoApp.exe "C:\Inno Install Output"
-delete C:\Users\petem\source\repos\RingSoft\Nuget\Release\CodeSign\RingSoft.WPFDbLookupDemoApp.exe
+del C:\Users\petem\source\repos\RingSoft\Nuget\Release\CodeSign\RingSoft.WPFDbLookupDemoApp.exe
 pause

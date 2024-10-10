21293df0f6dca4f1d7c395874f5fa42da2951aa3
Peter Ringering <peteman316@gmail.com>
2024-10-10 11:40:53 -0600

    Working

---
+++ RingSoft.DbLookup.App.WPFCore/Northwind/CustomersUserControl.xaml.cs
@@ -0,0 +1,33 @@
+﻿using RingSoft.DbLookup.Controls.WPF;
+using RingSoft.DbMaintenance;
+using System;
+using System.Windows.Controls;
+
+namespace RingSoft.DbLookup.App.WPFCore.Northwind
+{
+    /// <summary>
+    /// Interaction logic for CustomersUserControl.xaml
+    /// </summary>
+    public partial class CustomersUserControl
+    {
+        public CustomersUserControl()
+        {
+            InitializeComponent();
+        }
+
+        protected override DbMaintenanceViewModelBase OnGetViewModel()
+        {
+            return CustomersViewModel;
+        }
+
+        protected override Control OnGetMaintenanceButtons()
+        {
+            return ButtonsControl;
+        }
+
+        protected override DbMaintenanceStatusBar OnGetStatusBar()
+        {
+            return StatusBar;
+        }
+    }
+}

e78b979a35eb593b69e66347702be1dca77382f6
Peter Ringering <peteman316@gmail.com>
2024-10-11 08:47:41 -0600

    Working

--- RingSoft.DbLookup.App.WPFCore/NewMainWindow.xaml.cs
+++ RingSoft.DbLookup.App.WPFCore/NewMainWindow.xaml.cs
@@ -1,21 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using RingSoft.DataEntryControls.WPF;
+﻿using RingSoft.DataEntryControls.WPF;
 using RingSoft.DbLookup.App.Library;
-using RingSoft.DbLookup.App.WPFCore.Northwind;
-using RingSoft.DbLookup.Controls.WPF;
-using RingSoft.DbLookup.Lookup;
+using System.Windows.Controls;
 
 namespace RingSoft.DbLookup.App.WPFCore
 {

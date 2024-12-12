63c727b7f12006629af06261159d4bb2cccbb6f7
Peter Ringering <peteman316@gmail.com>
2024-12-12 16:17:02 -0700

    Working

--- RingSoft.DbMaintenance/DbMaintenanceViewModel.cs
+++ RingSoft.DbMaintenance/DbMaintenanceViewModel.cs
@@ -534,29 +534,13 @@ namespace RingSoft.DbMaintenance
                     var listPks = selectQuery.GetDataResult();
                     if (listPks.Any())
                     {
+                        var val = selectQuery.GetPropertyValue(0, column.FieldDefinition.PropertyName);
                         var newPrimaryKey = new PrimaryKeyValue(TableDefinition);
-                        newPrimaryKey.LoadFromIdValue(listPks[0].KeyValueFields[0].Value);
+                        newPrimaryKey.LoadFromIdValue(val);
                         selectedPrimaryKeyValue = newPrimaryKey;
                     }
                 }
 
-                //var joinTable = selectQuery.AddPrimaryJoinTable(JoinTypes.InnerJoin, TableDefinition.TableName);
-                //joinTable.AddJoinField(pkField.FieldName, primaryField.FieldName);
-                //selectQuery.AddSelectColumn(primaryField.FieldName);
-                //selectQuery.AddWhereItem(addViewPrimaryKeyValue.KeyValueFields[0].FieldDefinition.FieldName
-                //    , Conditions.Equals, value);
-
-                //var getTableResult = TableDefinition.Context.DataProcessor.GetData(selectQuery);
-                //if (getTableResult.ResultCode == GetDataResultCodes.Success)
-                //{
-                //    var table = getTableResult.DataSet.Tables[0];
-                //    if (table.Rows.Count > 0)
-                //    {
-                //        var newPrimaryKey = new PrimaryKeyValue(TableDefinition);
-                //        newPrimaryKey.LoadFromIdValue(table.Rows[0].GetRowValue(primaryField.FieldName));
-                //        selectedPrimaryKeyValue = newPrimaryKey;
-                //    }
-                //}
             }
             var result = addViewPrimaryKeyValue;
             switch (LookupAddViewArgs.LookupFormMode)
